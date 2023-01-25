8a3bfa608229cf5ae0eaf98d9bfd4652ddd4cead
John Doe <edixon20pes@gmail.com>
2023-01-25 12:54:18 -0400

    [Add] Style nav in index.css

---
+++ src/components/Nav.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import whiteLogo from '../images/tinder_logo_white.png'
+import colorLogo from '../images/color-logo-tinder.png'
+
+function Nav({minimal,authToken}) {
+
+  return (
+    <>
+      <nav>
+          <div className='logo-container'>
+            <img  className='logo' src={minimal ? colorLogo : whiteLogo} />
+          </div>
+
+          {!authToken && <button className='nav-button'>Log in</button>}
+      </nav>
+    </>
+  )
+}
+
+export default Nav
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Nav from "../components/Nav"
 
 function Home() {
+
+    const authToken = false;
+
+    const handleClick = () =>{
+        alert('click')
+    }
+
   return (
-    <div>Home</div>
+    <>
+    <Nav 
+        minimal={false}
+        authToken={false}
+    />
+    <div className='home'>
+        <h1>Swipe Rigth</h1>        
+        <button className='primary-button' onClick={handleClick}>  
+            {authToken ? 'Signout' : 'Create Account' }
+        </button>
+    </div>
+    </>
   )
 }
 
