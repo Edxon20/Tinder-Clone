73079cdb39fb91fc043b117628c8947d37576693
John Doe <edixon20pes@gmail.com>
2023-01-28 11:54:00 -0400

    [Finalize] Onboarding form

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -23,13 +23,19 @@ function Onboarding() {
   const {user_id, first_name, dob_day, dob_month, dob_year, show_gender, gender_identity, gender_interest, email, url, about, matches} = formData;
 
   const handleChange = (e) => {
+    
+    if(e.target.type == 'checkbox'){      
+      setFormData({
+        ...formData,
+        ["show_gender"]: !formData.show_gender,
+      })
+      return
+    }
 
-    const value = e.target.value
-
-    setFormData((prevState) =>({
-      ...prevState,
-      [name] : value
-    }))
+    setFormData({
+      ...formData,
+      [e.target.name] : e.target.value
+  })
   }
 
   return (
@@ -50,10 +56,10 @@ function Onboarding() {
             <input
               id='first_name'
               type="text"
-              name={first_name}
+              name="first_name"
               placeholder='First Name'
               required={true}
-              value={""}
+              value={first_name}
               onChange={handleChange}
             />
 
@@ -62,30 +68,30 @@ function Onboarding() {
               <input
                 id='dob_day'
                 type="number"
-                name={dob_day}
+                name="dob_day"
                 placeholder='DD'
                 required={true}
-                value={""}
+                value={dob_day}
                 onChange={handleChange}
               />
 
               <input
                 id='dob_month'
                 type="number"
-                name={dob_month}
+                name="dob_month"
                 placeholder='MM'
                 required={true}
-                value={""}
+                value={dob_month}
                 onChange={handleChange}
               />
 
               <input
                 id='dob_year'
                 type="number"
-                name={dob_year}
+                name="dob_year"
                 placeholder='YYYY'
                 required={true}
-                value={""}
+                value={dob_year}
                 onChange={handleChange}
               />
             </div>
@@ -97,33 +103,33 @@ function Onboarding() {
               <input
                 id='man-gender-identity'
                 type="radio"
-                name={gender_identity}
+                name="gender_identity"
                 placeholder='DD'
-                value="woman"
+                value="man"
                 onChange={handleChange}
-                checked={false}
+                checked={gender_identity === 'man'}
               />
               <label htmlFor='man-gender-identity'> Man</label>
 
               <input
                 id='woman-gender-identity'
                 type="radio"
-                name={gender_identity}
+                name="gender_identity"
                 placeholder='DD'
-                value="man"
+                value="woman"
                 onChange={handleChange}
-                checked={false}
+                checked={gender_identity === 'woman'}
               />
               <label htmlFor='woman-gender-identity'> Woman</label>
 
               <input
                 id='more-gender-identity'
                 type="radio"
-                name={gender_identity}
+                name="gender_identity"
                 placeholder='DD'
                 value="more"
                 onChange={handleChange}
-                checked={false}
+                checked={gender_identity === 'more'}
               />
               <label htmlFor='more-gender-identity'> More</label>
             </div>
@@ -132,47 +138,47 @@ function Onboarding() {
             <input
               id='show-gender'
               type="checkbox"
-              name={show_gender}
+              name="show_gender"
               placeholder='DD'
               onChange={handleChange}
-              checked={false}
+              checked={show_gender}
             />
 
             <label>Show me</label>
             <div className='multiple-input-container'>
-
+              
               <input
                 id='man-gender-interest'
                 type="radio"
-                name={gender_interest}
+                name="gender_interest"
                 placeholder='DD'
-                value="woman"
+                value="man"
                 onChange={handleChange}
-                checked={false}
+                checked={gender_interest === 'man'}
               />
-              <label htmlFor='man-gender-identity'> Man</label>
+              <label htmlFor='man-gender-interest'> Man</label>
 
               <input
                 id='woman-gender-interest'
                 type="radio"
-                name={gender_interest}
+                name="gender_interest"
                 placeholder='DD'
-                value="man"
+                value="woman"
                 onChange={handleChange}
-                checked={false}
+                checked={gender_interest === 'woman'}
               />
               <label htmlFor='woman-gender-interest'> Woman</label>
 
               <input
                 id='everyone-gender-interest'
                 type="radio"
-                name={gender_interest}
+                name="gender_interest"
                 placeholder='DD'
                 value="everyone"
                 onChange={handleChange}
-                checked={false}
+                checked={gender_interest === 'everyone'}
               />
-              <label htmlFor='more-gender-interest'> Every One</label>
+              <label htmlFor='everyone-gender-interest'> Every One</label>
             </div>
 
             <label htmlFor='about'>About me</label>
@@ -182,7 +188,7 @@ function Onboarding() {
               name="about"
               required={true}
               placeholder='I like cats and make money...'
-              value={""}
+              value={about}
               onChange={handleChange}
             />
             <input
@@ -197,11 +203,14 @@ function Onboarding() {
             <div className='photo-container'>
               <input
                 type="url"
-                name={url}
+                name="url"
                 id="url"
                 onChange={handleChange}
                 required={true}
               />
+              <div>
+                <img src={url}  alt="profile pic preview"/>
+              </div>
             </div>
           </section>
         </form>
