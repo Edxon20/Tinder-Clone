c50c1630b4a116071ede486fc5b548067289d713
John Doe <edixon20pes@gmail.com>
2023-01-28 19:33:16 -0400

    [TEST] connection to mongodb

--- server/index.js
+++ server/index.js
@@ -1,10 +1,36 @@
 const PORT = 8000
 const express = require('express');
+const { MongoClient } = require('mongodb')
+const url = 'mongodb+srv://Ragnar20:mongotinderclone123@cluster0.pz01qgs.mongodb.net/Cluster0?retryWrites=true&w=majority'
 
 const app = express()
 
 app.get('/', (req, res) => {
     res.json('Hello to my app')
 })
+
+app.post('/signup', (req, res) => {
+    res.json('Hello to my app')
+})
+// --------------------------------------------------------
+// these can be used to template
+app.get('/users', async(req, res) => {
+    const client = new MongoClient(url)
+
+    try{
+        await client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+
+        const returnedUsers = await users.find().toArray()
+        res.send(returnedUsers)
+    } finally{
+        await client.close()
+    }        
+})
+// -----------------------------------------------------------
+
+
+
 
 app.listen(PORT,() => console.log('Server running on PORT ' + PORT))
