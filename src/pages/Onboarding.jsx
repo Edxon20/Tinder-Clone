af229e27f8f74213778911b1310428c0ab120d85
John Doe <edixon20pes@gmail.com>
2023-01-27 20:47:28 -0400

    [Finalize] Form structure

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -8,6 +8,10 @@ function Onboarding() {
   //   e.preventDefault();
   // } 
 
+  const handleChange = () => {
+
+  }
+
   return (
     <>
       <Nav
@@ -18,14 +22,170 @@ function Onboarding() {
       <div className='onboarding'>
 
         <h2>CREATE ACCOUNT</h2>
-        
+
         <form onSubmit={e => e.preventDefault()} >
+          <section>
 
+            <label htmlFor='first_name'> First Name</label>
+            <input
+              id='first_name'
+              type="text"
+              name="first_name"
+              placeholder='First Name'
+              required={true}
+              value={""}
+              onChange={handleChange}
+            />
+
+            <label> Birthday </label>
+            <div className='multiple-input-container' >
+              <input
+                id='dob_day'
+                type="number"
+                name="dob_day"
+                placeholder='DD'
+                required={true}
+                value={""}
+                onChange={handleChange}
+              />
+
+              <input
+                id='dob_month'
+                type="number"
+                name="dob_month"
+                placeholder='MM'
+                required={true}
+                value={""}
+                onChange={handleChange}
+              />
+
+              <input
+                id='dob_year'
+                type="number"
+                name="dob_year"
+                placeholder='YYYY'
+                required={true}
+                value={""}
+                onChange={handleChange}
+              />
+            </div>
+
+            <label> Gender </label>
+            
+            <div className='multiple-input-container' >
+              
+            <input
+                id='man-gender-identity'
+                type="radio"
+                name="gender_identity"
+                placeholder='DD'
+                value="woman"
+                onChange={handleChange}
+                checked={false}
+              />
+              <label htmlFor='man-gender-identity'> Man</label>
+              
+              <input
+                id='woman-gender-identity'
+                type="radio"
+                name="gender_identity"
+                placeholder='DD'
+                value="man"
+                onChange={handleChange}
+                checked={false}
+              />
+              <label htmlFor='woman-gender-identity'> Woman</label>
+
+              <input
+                id='more-gender-identity'
+                type="radio"
+                name="gender_identity"
+                placeholder='DD'
+                value="more"
+                onChange={handleChange}
+                checked={false}
+              />
+              <label htmlFor='more-gender-identity'> More</label>             
+            </div>
+
+            <label htmlFor='show-gender'> Show gender on my profile</label>
+            <input
+                id='show-gender'
+                type="checkbox"
+                name="show_gender"
+                placeholder='DD'                
+                onChange={handleChange}
+                checked={false}
+              />
+
+              <label>Show me</label>
+              <div className='multiple-input-container'>
+
+              <input
+                id='man-gender-interest'
+                type="radio"
+                name="gender_interest"
+                placeholder='DD'
+                value="woman"
+                onChange={handleChange}
+                checked={false}
+              />
+              <label htmlFor='man-gender-identity'> Man</label>
+              
+              <input
+                id='woman-gender-interest'
+                type="radio"
+                name="gender_interest"
+                placeholder='DD'
+                value="man"
+                onChange={handleChange}
+                checked={false}
+              />
+              <label htmlFor='woman-gender-interest'> Woman</label>
+
+              <input
+                id='everyone-gender-interest'
+                type="radio"
+                name="gender_interest"
+                placeholder='DD'
+                value="everyone"
+                onChange={handleChange}
+                checked={false}
+              />
+              <label htmlFor='more-gender-interest'> Every One</label>      
+              </div>
+
+              <label htmlFor='about'>About me</label>
+              <input 
+                id='about'
+                type="text"
+                name="about"
+                required = {true}
+                placeholder='I like cats and make money...'
+                value = {""}
+                onChange={handleChange}
+              />
+              <input 
+                type="submit"
+                value="SUBMIT"
+
+              />
+          </section>
+
+          <section>
+            <label htmlFor='about'>Profile Profile</label>
+            <div className='photo-container'>
+              <input 
+                type="url"
+                name="url"
+                id="url"
+                onChange={handleChange}
+                required ={true}
+              />
+            </div>
+          </section>
         </form>
-
-
       </div>
-
     </>
 
 
